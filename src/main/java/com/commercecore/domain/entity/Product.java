cee3a39ee7e2437b96716ca83166b683bd5065a5
StefanoF02 <stefanof54@gmail.com>
2024-05-23 11:17:17 +0200

    Added nonnull to Product and extended with Category and Review

--- src/main/java/com/commercecore/domain/entity/Product.java
+++ src/main/java/com/commercecore/domain/entity/Product.java
@@ -5,6 +5,10 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.util.List;
+import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,14 +19,21 @@ public class Product {
     @Id
     private Long articleNumber;
 
+    @NonNull
     private String articleName;
 
+    @NonNull
     private Double price;
 
+    @NonNull
     private String producer;
 
+    @NonNull
     private Integer stock;
 
+    @NonNull
+    private Set<Category> categories;
 
+    private List<Review> reviews;
 
 }
