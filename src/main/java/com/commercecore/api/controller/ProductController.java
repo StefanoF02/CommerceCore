69382f9354e967f4c4648462c0600f6872d9b5f8
StefanoF02 <stefanof54@gmail.com>
2024-05-23 19:57:58 +0200

    Added Patch Get and Delete Methods to controller

--- src/main/java/com/commercecore/api/controller/ProductController.java
+++ src/main/java/com/commercecore/api/controller/ProductController.java
@@ -5,21 +5,38 @@ import com.commercecore.domain.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
-@RestController
+@RestController()
 public class ProductController {
 
     private final ProductService productService;
 
     // TODO Validation missing
     @PostMapping("/products")
-    public ResponseEntity<HttpStatus> saveProduct(@RequestBody Product product){
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product){
         this.productService.saveProduct(product);
+        return new ResponseEntity<Product>(product,HttpStatus.OK);
+    }
+
+    @PatchMapping("/products")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product){
+        this.productService.updateProduct(product);
+        return new ResponseEntity<Product>(product, HttpStatus.OK);
+    }
+
+    @GetMapping("/products/{articleNumber}")
+    public ResponseEntity<Product> getProduct(@PathVariable Integer articleNumber){
+        var product = this.productService.findByArtNumber(articleNumber);
+        return new ResponseEntity<Product>(product,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/products/{articleNumber}")
+    public ResponseEntity<HttpStatus> deleteProduct(@PathVariable Integer articleNumber){
+        var product = this.productService.deleteProduct(articleNumber);
         return new ResponseEntity<HttpStatus>(HttpStatus.OK);
     }
+
 
 }
