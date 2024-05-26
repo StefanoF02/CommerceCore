c54cb36a0a9588754d4d9376ffb32483a3f0f21c
StefanoF02 <stefanof54@gmail.com>
2024-05-26 17:16:27 +0200

    Added spring validation to other product fields.

--- src/main/java/com/commercecore/domain/entity/Product.java
+++ src/main/java/com/commercecore/domain/entity/Product.java
@@ -2,6 +2,8 @@ package com.commercecore.domain.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,28 +20,35 @@ public class Product {
     @Valid
 
     @Id
-    @Range(min = 100000000, max = 999999999, message = "Article number must be between 100000000 and 999999999.")
-    private Integer articleNumber;
+    @Range(min = 100000000000000L,
+            max = 999999999999999999L,
+            message = "Article number must be at least 15 and a maximum of 19 numbers long.")
+    @NotNull(message = "Article numberg must not be null.")
     private Long articleNumber;
 
     @NonNull
+    @NotBlank(message = "Article name must not be blank.")
+    @NotNull(message = "Article name must not be null.")
     private String articleName;
 
-    @NonNull
+
+    @NotNull(message = "Price must not be null.")
     private Double price;
 
+    @NotBlank(message = "Producer must not be blank.")
     @NonNull
     //@Size(min = 1, max = 9999)
     private String producer;
 
-    @NonNull
+    @Range(min = 1, message = "Stock must be at least 1.")
+    @NotNull(message = "Stock must not be null.")
     private Integer stock;
 
-    @NonNull
+    @NotNull(message = "Stock must not be null.")
     @Enumerated(EnumType.STRING)
     private Category categories;
 
     @OneToMany(mappedBy = "product")
-    private List<Review> reviews;
+    private List<Review> reviews = new ArrayList<>();
 
 }
