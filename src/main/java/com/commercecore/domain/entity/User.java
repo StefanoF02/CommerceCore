9cdda49f553a2cd8fb91bfb22bb6d056c6d21f8a
StefanoF02 <stefanof54@gmail.com>
2024-05-27 13:51:09 +0200

    Added User entity to project

---
+++ src/main/java/com/commercecore/domain/entity/User.java
@@ -0,0 +1,47 @@
+package com.commercecore.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @NotNull
+    private UUID uid;
+
+    @NotBlank(message = "Firstname be set.")
+    private String firstname;
+
+    @NotBlank(message = "Lastname must be set.")
+    private String lastname;
+
+    @NotBlank(message = "Email must not be set.")
+    private String email;
+
+    @NotBlank(message = "Telephone must be set.")
+    private Integer telephone;
+
+    @NotBlank(message = "age must be set.")
+    private Integer age;
+
+    @NotBlank(message = "Password must be set.")
+    private String password;
+
+    @NotBlank(message = "Role must be set.")
+    private String role;
+}
