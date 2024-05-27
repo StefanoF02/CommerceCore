203b063c296bee6d0d2d4292175884fadf717679
StefanoF02 <stefanof54@gmail.com>
2024-05-27 11:47:03 +0200

    Implemented ProductFactory for testing

---
+++ src/test/java/com/commercecore/it/factories/ProductFactory.java
@@ -0,0 +1,21 @@
+package com.commercecore.it.factories;
+
+import com.commercecore.domain.entity.Category;
+import com.commercecore.domain.entity.Currency;
+import com.commercecore.domain.entity.Product;
+
+public class ProductFactory {
+
+    public static Product buildFullProduct(){
+
+        return Product.builder()
+                .articleNumber(544695585556554L)
+                .articleName("MacBook Pro")
+                .price(1299.99)
+                .producer("Apple")
+                .currency(Currency.EURO)
+                .stock(2)
+                .categories(Category.TECH)
+                .build();
+    }
+}
