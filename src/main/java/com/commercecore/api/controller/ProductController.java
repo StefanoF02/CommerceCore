b3caf6515ea931c8352d9c668befd410dfb7c18b
StefanoF02 <stefanof54@gmail.com>
2024-05-27 15:35:32 +0200

    Changed return of Product class to ProductAo

--- src/main/java/com/commercecore/api/controller/ProductController.java
+++ src/main/java/com/commercecore/api/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.commercecore.api.controller;
 
+import com.commercecore.api.model.ProductAo;
 import com.commercecore.domain.entity.Product;
 import com.commercecore.domain.service.ProductService;
 import jakarta.validation.Valid;
@@ -18,27 +19,25 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/{articleNumber}")
-    public ResponseEntity<Product> getProduct(@PathVariable Long articleNumber){
+    public ResponseEntity<ProductAo> getProduct(@PathVariable Long articleNumber){
         var product = this.productService.findByArtNumber(articleNumber);
         return new ResponseEntity<>(product,HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity<List<Product>> getProductsByProducer(@RequestParam("producer") String producer){
-        List<Product> productsList = this.productService.findAllByProducer(producer);
+    public ResponseEntity<List<ProductAo>> getProductsByProducer(@RequestParam("producer") String producer){
+        List<ProductAo> productsList = this.productService.findAllByProducer(producer);
         return new ResponseEntity<>(productsList,HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<Product> saveProduct(@Valid @RequestBody Product product){
-        this.productService.saveProduct(product);
-        return new ResponseEntity<>(product,HttpStatus.OK);
+    public ResponseEntity<ProductAo> saveProduct(@Valid @RequestBody Product product){
+        return new ResponseEntity<>(this.productService.saveProduct(product),HttpStatus.OK);
     }
 
     @PatchMapping
-    public ResponseEntity<Product> updateProduct(@Valid @RequestBody Product product){
-        this.productService.updateProduct(product);
-        return new ResponseEntity<>(product, HttpStatus.OK);
+    public ResponseEntity<ProductAo> updateProduct(@Valid @RequestBody Product product){
+        return new ResponseEntity<>(this.productService.updateProduct(product), HttpStatus.OK);
     }
 
     @DeleteMapping("/{articleNumber}")
--- src/main/java/com/commercecore/domain/service/ProductService.java
+++ src/main/java/com/commercecore/domain/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.commercecore.domain.service;
 
+import com.commercecore.api.mapper.ProductMapper;
+import com.commercecore.api.model.ProductAo;
 import com.commercecore.domain.entity.Product;
 import com.commercecore.domain.repository.ProductRepository;
 import lombok.AllArgsConstructor;
@@ -8,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
@@ -17,26 +20,36 @@ public class ProductService {
     @Autowired
     private final ProductRepository productRepository;
 
-    public void saveProduct(Product product) {
+    @Autowired
+    private final ProductMapper productMapper;
+
+    public ProductAo saveProduct(Product product) {
         this.productRepository.saveOrUpdateProduct(product);
+        return productMapper.toApi(product);
     }
 
-    public void updateProduct(Product product) {
+    public ProductAo updateProduct(Product product) {
         if(!this.productRepository.exists(product.getArticleNumber())){
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,
                     "Product with article number " + product.getArticleNumber() + " not found");
         }
         this.productRepository.saveOrUpdateProduct(product);
+        return productMapper.toApi(product);
     }
 
-    public Product findByArtNumber(Long articleNumber) {
-        return productRepository.findByArtNumber(articleNumber).orElseThrow(
+    public ProductAo findByArtNumber(Long articleNumber) {
+        return productMapper.toApi( productRepository.findByArtNumber(articleNumber).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-                        "Product with article number " + articleNumber + " not found"));
+                        "Product with article number " + articleNumber + " not found")));
     }
 
-    public List<Product> findAllByProducer(String producer){
-        return productRepository.findAllByProducer(producer);
+    public List<ProductAo> findAllByProducer(String producer){
+        List<ProductAo> productsByProducer = new ArrayList<>();
+
+        productRepository.findAllByProducer(producer)
+                .forEach(product -> productsByProducer.add(productMapper.toApi(product)));
+
+        return productsByProducer;
     }
 
     public HttpStatus deleteProduct(Long articleNumber) {
